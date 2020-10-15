5eaa40da510d5a143da95fe4cfc1f8c74de17558
minyi <tug22058@temple.edu>
2020-10-15 18:32:24 -0400

    Finish to create the French string

--- app/src/main/java/edu/temple/colorchangingapp/PaletteActivity.java
+++ app/src/main/java/edu/temple/colorchangingapp/PaletteActivity.java
@@ -3,18 +3,14 @@ package edu.temple.colorchangingapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
-import android.widget.Spinner;
 
-import java.util.ArrayList;
 
 public class PaletteActivity extends AppCompatActivity {
 
