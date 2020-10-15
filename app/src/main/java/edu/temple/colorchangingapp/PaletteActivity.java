5fbaa4ba62154d0e776f0e6d74d47fdaf6d89e57
minyi <tug22058@temple.edu>
2020-10-15 18:37:00 -0400

    Create a new branch

--- app/src/main/java/edu/temple/colorchangingapp/PaletteActivity.java
+++ app/src/main/java/edu/temple/colorchangingapp/PaletteActivity.java
@@ -10,7 +10,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
-
+ 
 
 public class PaletteActivity extends AppCompatActivity {
 
